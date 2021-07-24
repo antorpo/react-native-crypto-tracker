d43090f53ebe3d87b6ecfe715e93685e42ac4302
Antonio Gonzalez Restrepo <antonio.gonzalez@xpertgroup.co>
2021-07-24 18:57:26 -0500

    Instalacion librerias
    Instalamos librerias para la navegacion @react-navigation/stack y @react-navigation/native y otras mas
     como la libreria de gestos

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'react-native-gesture-handler';
+
+/*
+    Importamos libreria especializada en los gestos como swipe up y asi,
+    react-native posee como manejar gestos pero no es lo mejor porque afecta
+    el rendimiento de la aplicacion.
+*/
 
 AppRegistry.registerComponent(appName, () => App);
